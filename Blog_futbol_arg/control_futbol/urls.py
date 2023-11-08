863c775762fc7683eed5bd10b59dcd455c48fbfc
Martin-Domenech <tinchodome@gmail.com>
2023-11-08 19:05:50 -0300

    Agregado el buscador de jugadores

--- Blog_futbol_arg/control_futbol/urls.py
+++ Blog_futbol_arg/control_futbol/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from control_futbol.views import (
-    listar_jugadores, crear_jugadores, listar_clubes, crear_clubes
+    listar_jugadores, crear_jugadores, listar_clubes, crear_clubes, listar_entrenadores, crear_entrenadores, buscar_jugadores,
 )
 
 urlpatterns = [
@@ -8,4 +8,7 @@ urlpatterns = [
     path("agregar-jugadores/",  crear_jugadores, name="agregar_jugadores"),
     path("clubes/",  listar_clubes, name="lista_clubes"),
     path("agregar-clubes/",  crear_clubes, name="agregar_clubes"),
+    path("entrenadores/",  listar_entrenadores, name="lista_entrenadores"),
+    path("agregar-entrenadores/",  crear_entrenadores, name="agregar_entrenadores"),
+    path("buscar-jugadores/",  buscar_jugadores, name="buscar_jugadores"),
 ]
--- Blog_futbol_arg/control_futbol/views.py
+++ Blog_futbol_arg/control_futbol/views.py
@@ -30,6 +30,18 @@ def listar_clubes(request):
     )
     return http_response
 
+def listar_entrenadores(request):
+    contexto = {
+        "entrenadores": Entrenador.objects.all(),
+    }
+    http_response = render(
+        request = request,
+        template_name = 'control_futbol/lista_entrenadores.html',
+        context=contexto,
+    )
+    return http_response
+
+
 def crear_jugadores(request):
     if request.method == "POST":
         formulario = JugadorFormulario(request.POST)
@@ -67,8 +79,8 @@ def crear_clubes(request):
             nombre = data["nombre"]
             ubicacion = data["ubicacion"]
 
-            jugador = Club(nombre=nombre, ubicacion=ubicacion)
-            jugador.save()
+            club = Club(nombre=nombre, ubicacion=ubicacion)
+            club.save()
 
             url_exitosa = reverse('lista_clubes')
             return redirect(url_exitosa)
@@ -81,3 +93,47 @@ def crear_clubes(request):
         context={'formulario': formulario}
     )
     return http_response
+
+ 
+def crear_entrenadores(request):
+    if request.method == "POST":
+        formulario = EntrenadorFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            nombre = data["nombre"]
+            apellido = data["apellido"]
+            club = data["club"]
+
+            entrenador = Entrenador(nombre=nombre, apellido=apellido, club=club)
+            entrenador.save()
+
+            url_exitosa = reverse('lista_entrenadores')
+            return redirect(url_exitosa)
+    else:
+        formulario=EntrenadorFormulario()
+        
+    http_response = render(
+        request=request,
+        template_name='control_futbol/agregar_entrenadores.html',
+        context={'formulario': formulario}
+    )
+    return http_response
+
+
+def buscar_jugadores(request):
+    if request.method == "POST":
+        data = request.POST
+        busqueda = data["busqueda"]
+        jugadores = Jugador.objects.filter(
+            Q(nombre__contains=busqueda) | Q(apellido__contains=busqueda)
+        )
+        contexto = {
+            "jugadores": jugadores,
+        }
+    http_response = render(
+        request=request,
+        template_name='control_futbol/lista_jugadores.html',
+        context=contexto,
+    )
+    return http_response    
